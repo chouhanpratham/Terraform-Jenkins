516a96aff39251852649edacdc5cd37ee531c953
Pratham Chouhan <109415931+chouhanpratham@users.noreply.github.com>
2025-04-04 15:14:34 +0530

    Create Project

---
+++ Terraform+Jenkins/terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "rg_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-jenkins"
+}
+
+variable "location" {
+  description = "Location of the resource group"
+  type        = string
+  default     = "East US"
+}
+
+variable "asp_name" {
+  description = "Name of the App Service Plan"
+  type        = string
+  default     = "asp-pratham-jenkins"
+}
+variable "as_name" {
+  description = "Name of the App Service Plan"
+  type        = string
+  default     = "webapijenkinspratham2222225"
+}
